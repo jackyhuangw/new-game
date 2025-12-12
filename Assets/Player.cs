d5afec44d6f1ef78fca937bf04f7a86c6def6af2
jackyhuangw <134223686+jackyhuangw@users.noreply.github.com>
2025-12-12 20:20:05 +0700

    Implemented idle/move/jump/fall animation to the player

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,21 +2,41 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Animator anim;
     private Rigidbody2D rb;
-    private float xInput;
+    
+    [Header("Movement Details")]
     [SerializeField] private float moveSpeed = 3.5f;
     [SerializeField] private float jumpForce = 8;
+    private float xInput;
+    private bool facingRight = true;
+
+    [Header("Collision Details")]
+    [SerializeField] private float groundCheckDistance;
+    private bool isGrounded;
+    [SerializeField] private LayerMask whatIsGround;
 
 
     private void Awake()
     {
+        anim = GetComponentInChildren<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
+        HandleCollision();
         HandleInput();
         HandleMovement();
+        HandleAnimations();
+        HandleFlip();
+    }
+
+    private void HandleAnimations()
+    {
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetFloat("xVelocity", rb.linearVelocity.x);
+        anim.SetFloat("yVelocity", rb.linearVelocity.y);
     }
 
     private void HandleInput()
@@ -34,6 +54,31 @@ public class Player : MonoBehaviour
 
     private void Jump()
     {
-        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        if(isGrounded)
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+    }
+
+    private void HandleCollision()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+    }
+
+    private void HandleFlip()
+    {
+        if (rb.linearVelocity.x > 0 && facingRight == false)
+            Flip();
+        else if (rb.linearVelocity.x < 0 && facingRight == true)
+            Flip();
+    }
+
+    private void Flip()
+    {
+        transform.Rotate(0, 180, 0);
+        facingRight = !facingRight;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, transform.position + new Vector3(0, -groundCheckDistance));
     }
 }
