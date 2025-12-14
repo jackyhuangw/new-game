5761735a36680e61c4f9e043ec6af397322b9538
jackyhuangw <134223686+jackyhuangw@users.noreply.github.com>
2025-12-14 14:25:36 +0700

    Clean up & refactoring

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class Player : Entity
+{
+    private float xInput;
+    private bool canJump = true;
+    [Header("Movement Details")]
+    [SerializeField] protected float moveSpeed = 3.5f;
+    [SerializeField] private float jumpForce = 8;
+
+    protected override void Update()
+    {
+        base.Update();
+        HandleInput();
+    }
+    protected override void HandleMovement()
+    {
+        if (canMove)
+            rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
+        else
+            rb.linearVelocity = new Vector2(0, rb.linearVelocity.y);
+    }
+
+    private void HandleInput()
+    {
+        xInput = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.W))
+            TryToJump();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            HandleAttack();
+    }
+
+    public override void EnableMovement(bool enable)
+    {
+        base.EnableMovement(enable);
+        canJump = enable;
+    }
+
+    private void TryToJump()
+    {
+        if (isGrounded && canJump)
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+    }
+}
